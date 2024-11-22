68ad2df335a8fb63fe92d777351c70ea542f6cf5
Jonah Unuafe <fejisfejiro@gmail.com>
2024-11-22 16:42:19 +0100

    add layout to group route

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen">
+      <section className="bg-brand p-10">
+        <div>
+          <Image
+            src=""
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto"
+          />
+
+          <div className="space-y-5 text-white">
+            <h1 className="h1">Manage your files the best way</h1>
+            <p className="body-1">
+              This is a place where you can store all your documents.
+            </p>
+          </div>
+        </div>
+        <Image
+          src="/illustration.png"
+          alt="files"
+          width={342}
+          height={342}
+          className="transition-all hover:rotate-2 hover:scale-105"
+        />
+      </section>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
