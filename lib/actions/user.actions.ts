07dd1fa15095d2399770d11cbb120deb61c100e3
Jonah Unuafe <fejisfejiro@gmail.com>
2024-11-25 13:35:15 +0100

    create sendEmailOTP and handleError helper functions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { createAdminClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 
@@ -17,6 +17,27 @@ const getUserByEmail = async (email: string) => {
   return result.total > 0 ? result.documents[0] : null;
 }
 
+// Helper function
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+}
+
+// Helper function
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+
+    return session.userId
+  } catch(error) {
+    handleError(error, "Failed to send email OTP")
+  }
+}
+
 const createAccount = async ({ fullName, email }: {fullName: string; email: string;}) => {
-  const existingUser = await getUserByEmail(email)
+  const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP({ email })
 }
