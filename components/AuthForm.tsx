04793c11d77a02e4e8c1af15c2d506d6323fadf8
Jonah Unuafe <fejisfejiro@gmail.com>
2024-11-26 08:24:13 +0100

    add Otp modal with alert-dialog

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,6 +18,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { createAccount } from '@/lib/actions/user.actions';
+import OtpModal from './OTPModal';
 
 type FormType = 'sign-in' | 'sign-up';
 
@@ -151,6 +152,14 @@ const AuthForm = ({ type }: { type: FormType }) => {
         </form>
       </Form>
       {/* OTP Verification */}
+      {
+        true && (
+          <OtpModal 
+            email={form.getValues("email")}
+            accountId={accountId} 
+          />
+        )
+      }
     </>
   );
 };
