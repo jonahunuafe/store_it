8a8282da29907ce94c1a2e331ac0dbe8437a47c3
Jonah Unuafe <fejisfejiro@gmail.com>
2024-11-30 21:40:05 +0100

    render files only if its files.total is greater than zero

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,8 +1,12 @@
 import Sort from '@/components/Sort';
+import { getFiles } from '@/lib/actions/file.actions';
+import { Models } from "node-appwrite";
 import React from 'react';
 
 const Page = async ({ params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || '';
+
+  const files = await getFiles();
 
   return (
     <div className="page-container">
@@ -22,7 +26,15 @@ const Page = async ({ params }: SearchParamProps) => {
       </section>
 
       {/* Dynamically render the files */}
-      
+      {files.total > 0 ? (
+        <section className="file-list">
+          {files.documents.map((file: Models.Document) => (
+            <h1 key={file.$id}>{file.name}</h1>
+          ))}
+        </section>
+      ) : (
+        <p className="empty-list">No files uploaded</p>
+      )}
     </div>
   );
 };
