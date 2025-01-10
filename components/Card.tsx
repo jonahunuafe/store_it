53df2aa4f953e3c05a437b6079a4d68f076660e9
Jonah Unuafe <fejisfejiro@gmail.com>
2025-01-10 09:21:28 +0100

    create FormattedDateTime component template

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 import { Models } from 'node-appwrite';
 import React from 'react';
 import Thumbnail from './Thumbnail';
+import { convertFileSize } from '@/lib/utils';
 
 const Card = ({ file }: { file: Models.Document }) => {
   return (
     <Link href={file.url} target="_blank" className="file-card">
-      {file.name}
       <div className="flex justify-between">
         <Thumbnail
           type={file.type}
@@ -14,14 +14,22 @@ const Card = ({ file }: { file: Models.Document }) => {
           url={file.url}
           className="!size-20"
           imageClassName="!size-11"
-        />
+          />
 
         <div className="flex flex-col items-end justify-between">
           ActionsDropdown ...
 
-          <p className="body-1">{}</p>
+          <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
       </div>
+
+      <div className="file-card-details">
+        <p className="subtitle-2 line-clamp-1">
+          {file.name}
+        </p>
+        
+      </div>
+
     </Link>
   );
 };
