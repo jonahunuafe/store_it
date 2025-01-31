46e30061212657fa9d3b4a33337efab655db4201
Jonah Unuafe <fejisfejiro@gmail.com>
2025-01-31 10:44:34 +0100

    implement the delete ui

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -103,6 +103,12 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
               onRemove={handleRemoveUser}
             />
           )}
+          {value === 'delete' && (
+            <p className="delete-confirmation">
+              Are you sure you want to delete?{` `}
+              <span className="delete-file-name">{file.name}</span>?
+            </p>
+          )}
         </DialogHeader>
 
         {['rename', 'delete', 'share'].includes(value) && (
