1f8d6432d9c4d31b1e3be75794c419363ffd0d2a
Jonah Unuafe <fejisfejiro@gmail.com>
2025-02-05 10:02:50 +0100

    implement search component

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+'use client';
+
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+import { Input } from './ui/input';
+import { useSearchParams } from 'next/navigation';
 
 const Search = () => {
+  const [query, setQuery] = useState('');
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query" || "")
+
+  useEffect(() => {
+    if(!searchQuery) {
+      
+    }
+  }, [])
+
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search">
+      <div className="search-input-wrapper">
+        <Image
+          src="/assets/icons/search.svg"
+          alt="Search"
+          width={24}
+          height={24}
+        />
+        <Input
+          value={query}
+          placeholder="Search..."
+          className="search-input"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Search;
