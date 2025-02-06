81f98c0526e489bae8ba987c5148e969ad09313c
Jonah Unuafe <fejisfejiro@gmail.com>
2025-02-06 14:19:28 +0100

    implement sort functionality

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 
 import {
@@ -8,16 +10,32 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 
+import { usePathname, useRouter } from 'next/navigation';
+import { sortTypes } from '@/constants';
+
 const Sort = () => {
+  const path = usePathname();
+  const router = useRouter();
+
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+
   return (
-    <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Theme" />
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
       </SelectTrigger>
-      <SelectContent>
-        <SelectItem value="light">Light</SelectItem>
-        <SelectItem value="dark">Dark</SelectItem>
-        <SelectItem value="system">System</SelectItem>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sort) => (
+          <SelectItem
+            key={sort.label}
+            className="shad-select-item"
+            value={sort.value}
+          >
+            {sort.label}
+          </SelectItem>
+        ))}
       </SelectContent>
     </Select>
   );
