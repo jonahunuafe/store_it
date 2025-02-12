09dd106bd8563ffc9e1666b7e6b038281571ac8f
Jonah Unuafe <fejisfejiro@gmail.com>
2025-02-12 08:59:32 +0100

    update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,8 +4,9 @@ import Sidebar from '@/components/Sidebar';
 import { getCurrentUser } from '@/lib/actions/user.actions';
 import { redirect } from 'next/navigation';
 import React from 'react';
-
 import { Toaster } from "@/components/ui/toaster"
+
+export const dynamic = 'force-dynamic';
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
